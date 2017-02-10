be4ae84f8caaed054c234a17896a70d6a603eff1
Petr Giloian <giloyanp@gmail.com>
2017-02-10 19:32:25 +0700

    Added 'durable' parameter to DefaultPublishStrategy constructor.

--- src/Lykke.RabbitMqBroker/Publisher/DefaultFnoutPublishStrategy.cs
+++ src/Lykke.RabbitMqBroker/Publisher/DefaultFnoutPublishStrategy.cs
@@ -5,16 +5,18 @@ namespace Lykke.RabbitMqBroker.Publisher
 {
     public class DefaultFnoutPublishStrategy : IRabbitMqPublishStrategy
     {
+        private readonly bool _durable;
         private readonly string _routingKey;
 
-        public DefaultFnoutPublishStrategy(string routingKey = "")
+        public DefaultFnoutPublishStrategy(string routingKey = "", bool durable = false)
         {
             _routingKey = routingKey;
+            _durable = durable;
         }
 
         public void Configure(RabbitMqSettings settings, IModel channel)
         {
-            channel.ExchangeDeclare(exchange: settings.QueueName, type: "fanout");
+            channel.ExchangeDeclare(exchange: settings.QueueName, type: "fanout", durable: _durable);
         }
 
         public void Publish(RabbitMqSettings settings, IModel channel, byte[] body)
