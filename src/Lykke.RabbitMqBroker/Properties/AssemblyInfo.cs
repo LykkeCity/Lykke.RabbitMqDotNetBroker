dff5e4559c516cfb1e628389e72c35c5d651dab4
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2017-07-26 15:20:15 +0000

    LWDEV-2385 Updated project information

--- src/Lykke.RabbitMqBroker/Properties/AssemblyInfo.cs
+++ src/Lykke.RabbitMqBroker/Properties/AssemblyInfo.cs
@@ -5,9 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lykke.RabbitMqBroker")]
+
+
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
