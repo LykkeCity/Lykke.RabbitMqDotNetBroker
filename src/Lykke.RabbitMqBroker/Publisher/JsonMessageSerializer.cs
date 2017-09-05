9bdf33c134151dfe112736ae6a18bd3c00a89160
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-09-06 00:39:47 +0300

    Serialized dates treated as UTC by default now

--- src/Lykke.RabbitMqBroker/Publisher/JsonMessageSerializer.cs
+++ src/Lykke.RabbitMqBroker/Publisher/JsonMessageSerializer.cs
@@ -6,20 +6,36 @@ namespace Lykke.RabbitMqBroker.Publisher
     public class JsonMessageSerializer<TMessage> : IRabbitMqSerializer<TMessage>
     {
         private readonly Encoding _encoding;
+        private readonly JsonSerializerSettings _settings;
 
         public JsonMessageSerializer() :
-            this(Encoding.UTF8)
+            this(Encoding.UTF8, null)
         {
         }
 
-        public JsonMessageSerializer(Encoding encoding)
+        public JsonMessageSerializer(Encoding encoding) :
+            this(encoding, null)
+        {
+        }
+
+        public JsonMessageSerializer(JsonSerializerSettings settings) :
+            this(null, settings)
+        {
+        }
+
+        public JsonMessageSerializer(Encoding encoding, JsonSerializerSettings settings)
         {
             _encoding = encoding;
+            _settings = settings;
         }
 
         public byte[] Serialize(TMessage model)
         {
-            var serialized = JsonConvert.SerializeObject(model);
+            var settings = _settings ?? new JsonSerializerSettings
+            {
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc
+            };
+            var serialized = JsonConvert.SerializeObject(model, settings);
 
             return _encoding.GetBytes(serialized);
         }
