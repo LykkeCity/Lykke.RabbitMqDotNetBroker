ebcc477fc160daedd2a6544013f9182545076239
Sergey Polonsky <polonsky.sergey@hotmail.com>
2018-07-18 13:26:15 +0300

    Added support of Protobuf as deserializer.

---
+++ src/Lykke.RabbitMqBroker/Publisher/ProtobufMessageSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using JetBrains.Annotations;
+
+namespace Lykke.RabbitMqBroker.Publisher
+{
+    /// <summary>
+    /// Uses Protobuf to serialize the message
+    /// </summary>
+    [PublicAPI]
+    public class ProtobufMessageSerializer<TMessage> : IRabbitMqSerializer<TMessage>
+    {
+        public byte[] Serialize(TMessage model)
+        {
+            using (var stream = new MemoryStream())
+            {
+                ProtoBuf.Serializer.Serialize(stream, model);
+
+                stream.Flush();
+
+                return stream.ToArray();
+            }
+        }
+    }
+}
---
+++ src/Lykke.RabbitMqBroker/Subscriber/ProtobufMessageDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using JetBrains.Annotations;
+
+namespace Lykke.RabbitMqBroker.Subscriber
+{
+    /// <summary>
+    /// Uses Protobuf to deserialize the message
+    /// </summary>
+    [PublicAPI]
+    public class ProtobufMessageDeserializer<TMessage> : IMessageDeserializer<TMessage>
+    {
+        public TMessage Deserialize(byte[] data)
+        {
+            using (var stream = new MemoryStream(data))
+            {
+                return ProtoBuf.Serializer.Deserialize<TMessage>(stream);
+            }
+        }
+    }
+}
