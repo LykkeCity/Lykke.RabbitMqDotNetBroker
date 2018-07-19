6bf4dd91e45d2d8f631e896a44d2cf43e0b2ad0b
Sergey Polonsky <polonsky.sergey@hotmail.com>
2018-07-19 14:43:21 +0300

    Code review fixes.

--- src/Lykke.RabbitMqBroker/Deduplication/IDeduplicator.cs
+++ src/Lykke.RabbitMqBroker/Deduplication/IDeduplicator.cs
@@ -1,9 +1,17 @@
 ﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
 
 namespace Lykke.RabbitMqBroker.Deduplication
 {
+    /// <summary>
+    /// Checks if the passed value was already received some time ago.
+    /// </summary>
+    [PublicAPI]
     public interface IDeduplicator
     {
+        /// <summary>
+        /// Check if the passed value was already received some time ago.
+        /// </summary>
         Task<bool> EnsureNotDuplicateAsync(byte[] value);
     }
 }
--- src/Lykke.RabbitMqBroker/Deduplication/InMemoryDeduplcator.cs
+++ src/Lykke.RabbitMqBroker/Deduplication/InMemoryDeduplcator.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Text;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace Lykke.RabbitMqBroker.Deduplication
 {
+    /// <summary>
+    /// Checks if the passed value was already received some time ago.
+    /// In-memory implementation.
+    /// </summary>
+    [PublicAPI]
     public class InMemoryDeduplcator : IDeduplicator
     {
         private static readonly IMemoryCache Cache = new MemoryCache(new MemoryCacheOptions());
         private static readonly System.Security.Cryptography.MD5 HashAlgorithm = System.Security.Cryptography.MD5.Create();
+        private readonly TimeSpan _expiration;
 
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="expiration">Cache expiration time.</param>
         public InMemoryDeduplcator(TimeSpan? expiration = null)
         {
-            if (!expiration.HasValue)
-                expiration = TimeSpan.FromDays(1);
+            _expiration = expiration ?? TimeSpan.FromDays(1);
         }
 
+        /// <summary>
+        /// Check if the passed value was already received some time ago.
+        /// </summary>
         public Task<bool> EnsureNotDuplicateAsync(byte[] value)
         {
             var hash = Encoding.UTF8.GetString(HashAlgorithm.ComputeHash(value));
@@ -24,7 +37,7 @@ namespace Lykke.RabbitMqBroker.Deduplication
             {
                 if (Cache.TryGetValue(hash, out object _))
                     return Task.FromResult(false);
-                Cache.Set(hash, true);
+                Cache.Set(hash, true, _expiration);
             }
             return Task.FromResult(true);
         }
