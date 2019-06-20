8ad662bf4dea1a106baab8cf449a1b910d0fa8f5
Sergey Nesterov <sergey.nesterov@lykke.com>
2019-06-20 18:09:10 +0300

    Added standard json publisher

---
+++ src/Lykke.RabbitMqBroker/Publisher/IRabbitPublisher.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Autofac;
+using Common;
+using JetBrains.Annotations;
+
+namespace Lykke.RabbitMqBroker.Publisher
+{
+    /// <summary>
+    /// Interface for 
+    /// </summary>
+    [PublicAPI]
+    public interface IRabbitPublisher<in TMessage> : IStartable, IStopable
+    {
+        Task PublishAsync(TMessage message);
+    }
+}
---
+++ src/Lykke.RabbitMqBroker/Publisher/JsonRabbitPublisher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Lykke.Common.Log;
+using Lykke.RabbitMqBroker.Subscriber;
+
+namespace Lykke.RabbitMqBroker.Publisher
+{
+    [PublicAPI]
+    public class JsonRabbitPublisher<TMessage> : IRabbitPublisher<TMessage>
+    {
+        private readonly ILogFactory _logFactory;
+        private readonly string _connectionString;
+        private readonly string _exchangeName;
+        private RabbitMqPublisher<TMessage> _rabbitMqPublisher;
+
+        public JsonRabbitPublisher(
+            ILogFactory logFactory,
+            string connectionString,
+            string exchangeName)
+        {
+            _logFactory = logFactory ?? throw new ArgumentNullException(nameof(logFactory));
+            _connectionString = connectionString;
+            _exchangeName = exchangeName;
+        }
+
+        public void Start()
+        {
+            var settings = RabbitMqSubscriptionSettings
+                .ForPublisher(_connectionString, _exchangeName)
+                .MakeDurable();
+
+            _rabbitMqPublisher = new RabbitMqPublisher<TMessage>(_logFactory, settings)
+                .SetSerializer(new JsonMessageSerializer<TMessage>())
+                .SetPublishStrategy(new DefaultFanoutPublishStrategy(settings))
+                .PublishSynchronously()
+                .Start();
+        }
+
+        public void Stop()
+        {
+            _rabbitMqPublisher?.Stop();
+        }
+
+        public void Dispose()
+        {
+            _rabbitMqPublisher?.Dispose();
+        }
+
+        public async Task PublishAsync(TMessage message)
+        {
+            await _rabbitMqPublisher.ProduceAsync(message);
+        }
+    }
+}
